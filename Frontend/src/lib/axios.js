5b35ea97339e01be5eed04f8b9df71889cf1c86e
Payal Das <payaldaskunu30@gmail.com>
2025-06-16 14:11:43 +0530

    initial commit

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chitchat-t9to.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
